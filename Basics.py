8a9154d91156181bf9279e6e24428e58da72c509
Preetam Kale <preetamkale@gmail.com>
2026-01-30 11:06:56 +0530

    Basics Session1

---
+++ Basics.py
@@ -0,0 +1,43 @@
+""" All programming boils down to a few basic operationss. This module covers them all.
+Programming is all about giving precise instructions to a computer to perform specific tasks.
+
+Computers are literal. 
+If you as a person to make a sandwich, you might say "make me a sandwich with lettuce, tomato, and cheese".
+
+A computer needs you to be more specific. 
+It needs to know exactly what type of bread, how much lettuce, how to slice the tomato, and what type of cheese.
+Every single step needs to be spelled out in detail."""
+
+#Few Basic Operations in Python
+
+#Store Information
+
+age = 25  # Storing an integer value
+name = "Alice"  # Storing a string value
+
+#We can reference these values later in the program
+print(f"{name} is {age} years old.")  # Output: Alice is 25 years old.
+
+#Make Decisions
+
+if age >= 18:
+    print(f"{name} is an adult.")
+else:
+    print(f"{name} is a minor.")
+
+#Repeat Actions
+
+"""repeat 10 times:
+    do pushups"""
+
+for i in range(5):
+    print(f"Doing pushups: {i+1}")
+
+#Perform Calculations
+
+total = price + tax
+
+#Python Syntax
+
+
+
